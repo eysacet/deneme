d2fd9edcffc32cca2a7db0275bf7362514189ea3
Eyyup Samil CETINKAYA <eysacet@gmail.com>
2023-01-16 18:47:54 +0100

    versiyon3

--- src/C01_gitGithub.java
+++ src/C01_gitGithub.java
@@ -5,5 +5,6 @@ public class C01_gitGithub {
         System.out.println("Oldu cok sukur");
         System.out.println("olmadi ama olacak");
         System.out.println("denemeye devam");
+        System.out.println("bugunun sonunu gorecegiz"   );
     }
 }
