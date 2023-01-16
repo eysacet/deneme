b163f6ddd13dc73b0a69aa27cfadd1598d3945af
Eyyup Samil CETINKAYA <eysacet@gmail.com>
2023-01-16 18:41:23 +0100

    version1

---
+++ src/C01_gitGithub.java
@@ -0,0 +1,9 @@
+public class C01_gitGithub {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+        System.out.println("olacak");
+        System.out.println("Oldu cok sukur");
+        System.out.println("olmadi ama olacak");
+        System.out.println("denemeye devam");
+    }
+}
---
+++ src/C02_.java
@@ -0,0 +1,6 @@
+public class C02_ {
+    public static void main(String[] args) {
+        System.out.println("git status versiyon durumumuzu soyler");
+    }
+
+}
